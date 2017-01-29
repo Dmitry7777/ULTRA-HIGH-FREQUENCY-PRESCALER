e7055517f74f4aebf2fe4420d22bafbd4cd8516c
Dmitry7777 <list-dmitrij@mail.ru>
2017-01-29 17:48:29 +0300

    Update ULTRA_HIGH_FREQUENCY_PRESCALER.h

--- ULTRA_HIGH_FREQUENCY_PRESCALER.h
+++ ULTRA_HIGH_FREQUENCY_PRESCALER.h
@@ -1,3 +1,23 @@
+/*
+Pin Number Symbol Function
+---------------------------
+1 - IN Input
+2 - VCC Power Supply Voltage
+3 - SW1 Divide Ratio Control Input Selecting Divide Ratio (See Divide Ratio Table)
+4 - OUT Output
+5 - GND Ground
+6 - SW2 Divide Ratio Control Input Selecting Divide Ratio (See Divide Ratio Table)
+7 - NC No Connection
+8 - IN Complementary Input
+
+Filename: ULTRA_HIGH_FREQUENCY_PRESCALER.h
+*/
+
+
 #ifndef ULTRA_HIGH_FREQUENCY_PRESCALER_H
 #define ULTRA_HIGH_FREQUENCY_PRESCALER_H
 
+#include<ULTRA_HIGH_FREQUENCY_PRESCALER.h>
+
+#define Input_Buffer
+#define Output_Buffer
